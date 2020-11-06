190da39e504f407fc8984723190b4a599dea74cc
Sindhuri Pallam Reddy <Sindhuri.Pallamreddy@regence.com>
2020-11-06 00:41:25 +0000

    week 3 - Jinja Primer, Macros, and Packages

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,14 +1,16 @@
 with payment as (
 
     select
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-        amount,
-        status,
-        created as created_at
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
 
-    from {{ source ('stripe' , 'payment')}}
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount', 4) }} as amount,
+    created as created_at
+
+from {{ source('stripe','payment') }}
 
 )
 
